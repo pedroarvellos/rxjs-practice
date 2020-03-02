9499180438232ab5d1bd97bb0d13422faa96be19
pgsantosarvellos <pedrogoncalves.santosarvellos@altran.com>
2020-03-02 14:23:00 +0000

    Add dinamically a value to HTML

--- src/code.ts
+++ src/code.ts
@@ -6,4 +6,7 @@ const countOutput = document.getElementById('count-output')
 
 fromEvent(button, 'click')
     .pipe(scan(count => count + 1, 0))
-    .subscribe(count => countOutput.innerHTML = countOutput.innerHTML + count);
+    .subscribe(count => {
+        countOutput.innerHTML = '';
+        countOutput.innerHTML = countOutput.innerHTML + count;
+    });
