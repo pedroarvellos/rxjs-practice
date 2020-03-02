aec2a2cb70a0d6304ab044b193963d3de2ac238e
pgsantosarvellos <pedrogoncalves.santosarvellos@altran.com>
2020-03-02 15:41:01 +0000

    Observable explanation

--- src/observables.ts
+++ src/observables.ts
@@ -1,19 +1,56 @@
 import { Observable } from 'rxjs';
 
+/*
+    Description:
+
+    The Observables follows the Push protocol of how the data flows. In other orders,
+    the Observables are functions that produces the data, and "advertises" the
+    consumer functions that the data is produces, so the consumer reacts according to
+    this information.
+
+    In the example below, the observable produces data, and "advertises" every time
+    something is produced. When the observable calls the function .next, it tells the
+    consumer, something is ready. So the consumer, which in this case is the .subscribe function,
+    reacts to the data it is produced by executing the function .next.
+
+    When the flow is finished in the Observable producer, it executes the funtion
+    .complete, so the consumer reacts executing the .complete.
+*/
+
+const unorderedList = document.getElementById('observables-list');
+const loading = document.getElementById('observables-loading');
+
+function changeLoadingState(state: string) {
+  loading.innerHTML = '';
+  loading.innerHTML = loading.innerHTML + state;
+}
+
 const observable = new Observable(subscriber => {
-  subscriber.next(1);
-  subscriber.next(2);
-  subscriber.next(3);
+  changeLoadingState('loading');
+  subscriber.next('First Item');
+  subscriber.next('Second Item');
   setTimeout(() => {
-    subscriber.next(4);
-    subscriber.complete();
+    subscriber.next('Third Item');
   }, 1000);
+  setTimeout(() => {
+    subscriber.next('Fourth Item');
+    subscriber.complete();
+  }, 2000);
 });
 
-console.log('just before subscribe');
 observable.subscribe({
-  next(x) { console.log('got value ' + x); },
-  error(err) { console.error('something wrong occurred: ' + err); },
-  complete() { console.log('done'); }
+  next(x: string) {
+    const node = document.createElement("li");
+    const textNode = document.createTextNode(x);
+
+    node.appendChild(textNode);
+
+    unorderedList.appendChild(node)
+  },
+  error(err) {
+    console.error('something wrong occurred: ' + err);
+  },
+  complete() {
+    changeLoadingState('complete');
+  }
 });
-console.log('just after subscribe');
