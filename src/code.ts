a79257d001c7d6141dab958949e5b9278a893747
pgsantosarvellos <pedrogoncalves.santosarvellos@altran.com>
2020-03-02 14:19:12 +0000

    Config Webpack, TypeScript and start RxJS code

---
+++ src/code.ts
@@ -0,0 +1,9 @@
+import { fromEvent } from 'rxjs';
+import { scan } from 'rxjs/operators';
+
+const button = document.querySelector('button');
+const countOutput = document.getElementById('count-output')
+
+fromEvent(button, 'click')
+    .pipe(scan(count => count + 1, 0))
+    .subscribe(count => countOutput.innerHTML = countOutput.innerHTML + count);
