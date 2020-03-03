b27412f2464a46e13e1b51a0a709a265cb9faa5c
pgsantosarvellos <pedrogoncalves.santosarvellos@altran.com>
2020-03-03 14:30:58 +0000

    Include creation operator

--- src/operators.ts
+++ src/operators.ts
@@ -1,36 +1,53 @@
-import { of } from 'rxjs';
+import { of, interval } from 'rxjs';
 import { map, first } from 'rxjs/operators';
 
 /*
-Operators are functions, and there are two kinds of
-operators: pipeable operators and creation operators.
+    Operators are functions, and there are two kinds of
+    operators: pipeable operators and creation operators.
+*/
 
-The pipeable operators are functions that can be wrapped
-inside the .pipe function, and the behaviour of these
-operators is not to change the existing Observable, but
-return a new one based on the first Observable. In other
-words, the previous Observable will stay unmodified, but
-a updated Observable will be returned.
+/*
+    Pipeable operators:
 
-The creation operators will be explained later.
+    The pipeable operators are functions that can be wrapped
+    inside the .pipe function, and the behaviour of these
+    operators is not to change the existing Observable, but
+    return a new one based on the first Observable. In other
+    words, the previous Observable will stay unmodified, but
+    a updated Observable will be returned.
 
-In the example above, I am using the function .of, which is
-a creation observable. This function is returning the values
-1, 2 and 3 as emitting observables, one by another. It is like
-emitting three .next functions in a row.
+    In the example above, I am using the function .of, which is
+    a creation observable. This function is returning the values
+    1, 2 and 3 as emitting observables, one by another. It is like
+    emitting three .next functions in a row.
 
-The function map takes the retuning observable, transforms the
-value of this observable in another one (the value times the value),
-and returns another observable with the new value.
+    The function map takes the retuning observable, transforms the
+    value of this observable in another one (the value times the value),
+    and returns another observable with the new value.
 
-After this, the subscribe method takes this observable and receives
-this last Observable, logging the result. This action will happen
-3 times, one for the returning number 1, another for the returning number 2
-and another for the returning number 3.
+    After this, the subscribe method takes this observable and receives
+    this last Observable, logging the result. This action will happen
+    3 times, one for the returning number 1, another for the returning number 2
+    and another for the returning number 3.
+
+    .pipe is a function to concat other functions,
+        pipe make things a bit easier, because you avoid
+        wrapping many functions and losing control, like:
+        op4()(op3()(op2()(op1()(obs))))
+
+        With pipe it would be something like:
+        obs.pipe(
+            op1(),
+            op2(),
+            op3(),
+            op3(),
+        )
+
 */
 
 const unorderedListMap = document.getElementById('operators-map-list');
 const unorderedListFirst = document.getElementById('operators-first-list');
+const numbersByInterval = document.getElementById('operators-interval-list');
 
 map(x => <any>x * <any>x)(of(1, 2, 3)).subscribe(v => {
     const node = document.createElement('li');
@@ -56,3 +73,23 @@ first()(of(1, 2, 3)).subscribe(v => {
 
     unorderedListFirst.appendChild(node)
 });
+
+/*
+    The creation Operators:
+
+    They are functions that can create Observables.
+    
+    The function below creates one Observable that
+    emmits one value in the interval of 5 seconds.
+*/
+
+const observable = interval(5000); // in milli
+
+observable.subscribe(x => {
+    const node = document.createElement('span');
+    const textNode = document.createTextNode(' ' + x);
+
+    node.appendChild(textNode);
+
+    numbersByInterval.appendChild(node)
+})
